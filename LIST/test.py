67aef2b7bd22c3a1ef1622e5c58b8597e960b3b3
krishna26605 <krishnaanikam26@gmail.com>
2025-09-21 10:06:07 +0530

    added list content

--- LIST/test.py
+++ LIST/test.py
@@ -1,1 +1,15 @@
 # list
+
+# list=["A" , "B" , "C" , "D" , "E" , "F"]
+# n= len(list)
+# for elem in range(n):
+#     print(list[elem], "The positive index is {} and negative index is {}".format(elem,elem-n))
+    
+
+# l=[]
+# for elem in range(101):
+#     if elem%10==0:
+#         l.append(elem)
+# print(l)
+
+
