35b09840c43428150030cf4e0634a11525a7bcc9
krishna26605 <krishnaanikam26@gmail.com>
2025-09-22 07:22:13 +0530

    added list content..

--- LIST/test.py
+++ LIST/test.py
@@ -13,3 +13,36 @@
 # print(l)
 
 
+
+
+# list=[[1,2,3],[4,5,6],[7,8,9]]
+# print("ROW WISE...")
+# for elem in list:
+#     print(elem)
+
+# print("MATRIX WISE...")
+# for i in range(len(list)):
+#     for j in range(len(list[i])):
+#         print(list[i][j], end=" ")
+#     print()
+
+
+
+# list=[]
+# for elem in range(1,11):
+#     list.append(elem*elem)
+# print(list)
+
+
+# l1= [elem*elem for elem in range(1,11)]            #square number from 1-10  #LIST COMPREHENSIONS..
+# l2=[elem for elem in l1 if elem%2==0]              #even number from that squares of 1-10
+# print("LIST 1 :" , l1)
+# print("LIST 2 :" , l2)
+
+
+# string= "the quick brown fox jumps over the lazy dog".split()
+# # print(string)
+
+# l2= [w.swapcase() for w in string]
+# print(" ".join(l2))
+
