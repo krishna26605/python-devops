542cf4ef89acfc7c940679b23e286dd5239db591
krishna26605 <krishnaanikam26@gmail.com>
2025-09-25 09:47:07 +0530

    added set content...

--- SET/test.py
+++ SET/test.py
@@ -1,9 +1,16 @@
-dict={}
-n= int(input("Enter the number of students:"))
-i=1
-while i<=n:
-    name=input("Enter the student name: ")
-    marks=input("Enter the student marks: ")
-    dict[name]=marks
-    i=i+1
-print(dict)
+# dict={}
+# n= int(input("Enter the number of students:"))
+# i=1
+# while i<=n:
+#     name=input("Enter the student name: ")
+#     marks=input("Enter the student marks: ")
+#     dict[name]=marks
+#     i=i+1
+# print(dict)
+
+
+
+# s= {1, 2, 3,4, 5,6}
+# l={"name":"Krishna", 100:"durga"}
+# s.update(l.values())
+# print(s)
