d9aee3132e7e396f4d33948527de56a5f35cefbf
krishna26605 <krishnaanikam26@gmail.com>
2025-09-26 14:08:04 +0530

    function concept added..

---
+++ FUNCTION[D]/test.py
@@ -0,0 +1,49 @@
+# def f1():
+#     print("HELLO...")
+
+# # f1()
+# print(f1())
+
+
+
+# def evenodd(n):
+#     if n%2==0:
+#         print("EVEN: ",n)
+#     else:
+#         print("odd: ", n)
+
+# evenodd(10.4)
+# evenodd(10.5)
+# evenodd(11)
+# evenodd(12)
+
+
+# def factorial(n):
+#     result=1
+#     while n>=1:
+#         result= result *n
+#         n=n-1
+#     return result
+
+# for fact in range(1, 6):
+#     print("The factorial of {} is {}".format(fact,factorial(fact)))
+
+
+
+# def wish(name , msg):
+#     print("HELLO", name , msg)
+
+
+# wish(name="Krishna" , msg="How r u?")
+
+# wish(msg="GOOD MORNING", "KRISHNA")
+
+
+
+
+
+def wish(name="Guest"):
+    print("Hello" , name, "Good Evening")
+
+wish("krishna")
+wish()
