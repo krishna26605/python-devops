57d45125d9f99f7fe5d0d7544e2c50009b475d66
krishna26605 <krishnaanikam26@gmail.com>
2025-09-30 20:00:35 +0530

    exception handling started...

---
+++ EXCEPTION__HANDLING/test.py
@@ -0,0 +1,16 @@
+# print("Hello started...")
+# try:
+#     print(10/0)
+# except BaseException:                              # we can use ZeroDivionError as well
+#     print(10/2)
+
+
+# print("Ended....")
+
+
+# HOW TO PRINT EXCEPTION INFORMATION
+
+# try:
+#     print(10/0)
+# except ZeroDivisionError as msg:
+    # print("We got an exception", msg) ------------------>  .We got an exception division by zero
